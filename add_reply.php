7e7eb678beceb24b8ad3b3fd8c31ac4bf8bc561c
Satan <i@satan.so>
2012-10-28 20:27:04 +0800

    filter

--- add_reply.php
+++ add_reply.php
@@ -1,12 +1,15 @@
 <?php
-require_once('ReplyModel.php');
+if (filter_has_var(INPUT_POST, 'content')) {
+	require_once('ReplyModel.php');
 
-$reply = new Reply();
+	$reply = new Reply();
 
-$reply->setContent($_POST['content']);
-$reply->setCreateTime(time());
+	//$reply->setContent($_POST['content']);
+	$reply->setContent(filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRING));
+	$reply->setCreateTime(time());
 
-ReplyModel::add($reply);
+	ReplyModel::add($reply);
+}
 
 header("Location:index.php");
 ?>
